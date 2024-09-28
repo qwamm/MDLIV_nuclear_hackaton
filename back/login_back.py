d431cad059e40a565f71a784b9f4d231a15c7912
kargamant <firexholms@gmail.com>
2024-09-28 22:56:22 +0300

    Made access token retrievement from github

--- back/login_back.py
+++ back/login_back.py
@@ -2,26 +2,22 @@ import fastapi as fs
 import httpx
 from fastapi.staticfiles import StaticFiles
 
-HOST="127.0.0.1"
-PORT="5173"
 log_app=fs.FastAPI()
-log_app.mount("/", StaticFiles(directory="", html=True))
 
 @log_app.get("/")
-def redirect_from_github(code: str = ""):
+def redirect_from_github(code: str=""):
     #example code=4d11b545c16e378ad2bb
     if code!="":
-        httpx.post("https://github.com/login/oauth/access_token", data={
+        response=httpx.post("https://github.com/login/oauth/access_token", params={
             'client_id': 'Iv23liDMCg3eeIfAIYy1',
             'client_secret': 'acd0e734f8cd0e64db104c4f210d71b63b7214db',
             'code': code,
-            'redirect_uri': '/'
+            'redirect_uri': 'http://localhost:5173/'
         })
-
-@log_app.get("/")
-def default_greetings():
-    return {'greetings':"hello from MDLIV"}
-
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run(log_app, host=HOST, port=PORT)
+        token=response.content.decode().split("&")[0].split("=")[-1]
+        if token=="bad_verification_code":
+            return {"error": token}
+        else:
+            return {"token": response.content.decode().split("&")[0].split("=")[-1]}
+    else:
+        return {'greetings': "hello from MDLIV"}
